cbcab09e4d872a078362801c61a43f2ab6db2fc4
szevasz <163414309+TPatya@users.noreply.github.com>
2024-12-09 13:57:04 +0100

    Add project files.

---
+++ KeszthelySprint/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace KeszthelySprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new();
+
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new Versenyzo(sor));
+            }
+            Console.WriteLine($"2.Feladat: \t A Versenyt {versenyzok.Count()} versenyző fejezte be");
+
+            int junior = 0;
+
+            foreach (var j in versenyzok)
+            {
+                if (j.Kategoria == "elit junior")
+                {
+                    junior += 1;
+                }
+            }
+            Console.WriteLine($"3.Feladat \t A versnyzők száma Az \"elit junior\" kategóriában: {junior} Fő");
+
+            int atlag = 0;
+            foreach (var s in versenyzok)
+            {
+                atlag += DateTime.Now.Year - s.Szuletes;
+            }
+            Console.WriteLine($"4. feladat \t Az átlag életkor: {atlag / versenyzok.Count():0.0} év");
+        }
+    }
+}
---
+++ KeszthelySprint/Versenyzo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeszthelySprint
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+
+        public int Szuletes { get; set; }
+
+        public int Rajtszam { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Kategoria { get; set; }
+
+        public string uszas { get; set; }
+
+        public string elsodepo { get; set; }
+
+        public string kerekpar { get; set; }
+
+        public string masodikdepo { get; set; }
+
+        public string futas { get; set; }
+
+
+
+        public Versenyzo(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Nev = s[0];
+
+            Szuletes = int.Parse(s[1]);
+
+            Rajtszam = int.Parse(s[2]);
+
+            Gender = s[3];
+
+            Kategoria = s[4];
+
+            uszas = s[5];
+
+            elsodepo = s[6];
+
+            kerekpar = s[7];
+
+            masodikdepo = s[8];
+
+            futas = s[9];
+
+
+        }
+    }
+}
